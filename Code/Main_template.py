ee2cccedf4adf6b08139843bbce69f51dc62833d
Hans-Christian Ruiz Euler <hans.ruiz.euler@hotmail.com>
2019-01-14 10:27:59 +0100

    TODO added

--- Code/Main_template.py
+++ Code/Main_template.py
@@ -6,17 +6,18 @@ from SmoothingProblem_template import LQ_Problem as SP
 from APIS import APIS as Smoother
 
 comm = MPI.COMM_WORLD
-
-Iters = 25
 case_identifier = sys.argv[1]
 
+#TODO: move meta_params definition into a module to keep main template clean
 meta_params = {}
-meta_params["Iters"] = Iters
+meta_params["Iters"] = 25
 meta_params["steps_between_obs"] = 100
 meta_params["N_particles"] = 200     # particles per worker
 meta_params["learning_rate"] = 0.2
 meta_params["anneal_threshold"] = 10 # in number of particles
 meta_params["anneal_factor"] = 1.1
+
+Iters = meta_params["Iters"]
 
 #Instantiate objects
 smproblem = SP(comm,meta_params)
